13b6f4627e5ef5ade16c0430dc72690c2ffa3b75
Anshul Gupta <anshul.d.gupta@pwc.com>
2026-07-23 08:55:34 +0000

    audit fix

--- finance/egov/egov-egf/src/main/java/org/egov/utils/FinancialConstants.java
+++ finance/egov/egov-egf/src/main/java/org/egov/utils/FinancialConstants.java
@@ -62,7 +62,7 @@ public interface FinancialConstants {
 	public static final String alphaNumericwithspecialcharForContraWOAndSupplierName = "[a-zA-Z0-9 @#&/\\\\()-.*,\":]+";
 	public static final String alphaNumericwithoutspecialchar = "[0-9a-zA-Z/]+";
 	public static final String numericwithoutspecialchar = "[0-9-]+";
-	public static final String ALPHANUMERICWITHALLSPECIALCHAR = "[0-9a-zA-Z_@./#&+-/!(){}\",^$%*|=;:<>?`~ ]+";
+	public static final String ALPHANUMERICWITHALLSPECIALCHAR = "[0-9a-zA-Z_@./#&+-/!(){}\",^$%*|=;:?`~ ]+";
 	public static final int AMOUNT_SCALE = 2;// scale value for BigDecimal
 												// having amounts
 	// bill types ie module types used in egw_status
